a6818c2ca0100e5cf656176f3a2243ca6fe7ca26
godspeedelbow <eelke@boezeman.com>
2018-05-24 18:45:26 +0200

    added article 1005

---
+++ src/assets/data/logboek/1005.js
@@ -0,0 +1,33 @@
+import photo1 from "../photos/1005-22-mei-eastbourne-tussen-de-modderbanken-800px.jpg";
+import photo2 from "../photos/1005-22-mei-in-de-sluis-van-eastbourne-800px.jpg";
+
+export default {
+  id: "1005",
+  title: "Getijvaren",
+  created: "22 mei 2018",
+  location: "Eastbourne",
+  geolocation: {
+    lat: 50.925,
+    lng: 0.46777777777777774
+  },
+  paragraphs: [
+    `Na twee dagen en nachten varen we de Zuid-Engelse haven Eastbourne binnen. Het is daar
+    bijna laag water. Er is een smalle geul tussen modderbanken, zodat we de haven nog wel
+    kunnen bereiken. Vlak voor de sluis staat de dieptemeter op nul, we lopen bijna vast. Tien
+    minuten later en het was niet meer gelukt om binnen te lopen.`,
+    `Eenmaal in de sluis stort het water vanuit de haven naar beneden en wij worden met een
+    rotgang vier meter omhoog getild. Dan gaan de sluisdeuren open. We zijn binnen.`,
+    `Komende weken willen we meer havens aan de zuidkust bezoeken. Havens die ook allemaal
+    droogvallen. Dat doet een groot beroep op onze rekenvaardigheden.`,
+  ],
+  photos: [
+    {
+      src: photo1,
+      title: 'Een local weet precies hoe hij moet varen zonder vast te lopen.'
+    },
+    {
+      src: photo2,
+      title: 'Net op tijd binnen!'
+    },
+  ]
+};
--- src/data.js
+++ src/data.js
@@ -4,8 +4,10 @@ import article1001 from "./assets/data/logboek/1001";
 import article1002 from "./assets/data/logboek/1002";
 import article1003 from "./assets/data/logboek/1003";
 import article1004 from "./assets/data/logboek/1004";
+import article1005 from "./assets/data/logboek/1005";
 
 const articles = [
+  article1005,
   article1004,
   article1003,
   article1002,
