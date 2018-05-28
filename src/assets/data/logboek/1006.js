d67c955e45364351aacd611e4d65c23086a5d039
godspeedelbow <eelke@boezeman.com>
2018-05-28 21:03:09 +0200

    article 1006

---
+++ src/assets/data/logboek/1006.js
@@ -0,0 +1,38 @@
+import photo1 from "../photos/1006-23-mei-reparatie-horloge-800px.jpg";
+import photo2 from "../photos/1006-23-mei-pier-eastbourne-800px.jpg";
+
+export default {
+  id: "1006",
+  title: "Niet zwart-wit",
+  created: "27 mei 2018",
+  location: "Portsmouth",
+  geolocation: {
+    lat: 51.03805556,
+    lng: 1.15972222
+  },
+  paragraphs: [
+    `Het is een gecompliceerd beeld dat je hier van de Engelsen krijgt. In de straten leven rijk en
+    arm, chique en sjofel naast elkaar. Mooi gerestaureerde gebouwen (zoals de pier in
+    Eastbourne) worden slecht onderhouden, reparaties worden niet of half uitgevoerd, zodat
+    binnen mum van tijd het verval weer intreedt.`,
+    `Ook in de havens zie je dat dualisme: zeer luxueuze jachten die een fortuin kosten, liggen
+    gebroederlijk (of gezusterlijk) langs kleine jachtjes waar blijkbaar jarenlang niks aan gedaan
+    is, met groen uitgeslagen lijnen op het dek, afgebladderde verf en enorme wolken algen
+    onder de waterlijn.`,
+    `In drukke winkelstraten kijk je je ogen uit. Glimmende etalages, waar de goedkoopste Rolex
+    voor minstens 3500 pond over de toonbank gaat. Sla je de hoek om, daal je via een smal
+    trapje af naar een onooglijk winkeltje, waar een uiterst vriendelijke meneer je Casio-horloge
+    secuur van een nieuw bandje voorziet. En ook nog de batterij vernieuwt voor de wereldse
+    totaalprijs van 8 pond.`,
+  ],
+  photos: [
+    {
+      src: photo1,
+      title: 'Een hele grote loupe voor een heel klein batterijtje. 20 minuten werk.'
+    },
+    {
+      src: photo2,
+      title: 'Zo op het oog ziet de pier in Eastbourne er prachtig uit. Dichterbij valt dat vies tegen.'
+    },
+  ]
+};
--- src/data.js
+++ src/data.js
@@ -5,8 +5,10 @@ import article1002 from "./assets/data/logboek/1002";
 import article1003 from "./assets/data/logboek/1003";
 import article1004 from "./assets/data/logboek/1004";
 import article1005 from "./assets/data/logboek/1005";
+import article1006 from "./assets/data/logboek/1006";
 
 const articles = [
+  article1006,
   article1005,
   article1004,
   article1003,
