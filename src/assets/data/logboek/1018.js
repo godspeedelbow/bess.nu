f71453703436f9651d6354092d5c7e5e29141b38
godspeedelbow <eelke@boezeman.com>
2018-07-21 11:33:36 +0200

    article 1018

---
+++ src/assets/data/logboek/1018.js
@@ -0,0 +1,43 @@
+import photo1 from "../photos/1018-20-juli-2018-schelpen-vissen-bij-laag-water-800px.jpg";
+import photo2 from "../photos/1018-20-juli-2018-de-korf-wordt-over-de-bodem-getrokken-800px.jpg";
+import photo3 from "../photos/1018-20-juli-2018-de-vangst-gaat-in-mandjes-die-op-de-visafslag-verhandeld-worden-800px.jpg";
+import photo4 from "../photos/1018-20-juli-2018-bij-hoog-water-ligt-de-haven-weer-vol;-de-korven-klaar-voor-de-volgende-dag-800px.jpg";
+import photo5 from "../photos/1018-20-juli-2018-ook-bewoners-zoeken-schelpen-op-het-strand-800px.jpg";
+
+export default {
+  id: "1018",
+  title: "Grote en kleine vissers (2)",
+  created: "20 juli 2018",
+  location: "Portonovo",
+  geolocation: {
+    lat: 42.40305556,
+    lng: -8.82472222
+  },
+  paragraphs: [
+    `De vangst van vissen en schelpen is een belangrijke bron van inkomsten in de Ria's van Spanje. Kleine open bootjes zijn aan het werk met laag water.`,
+    `In zo'n klein bootje zitten twee vissers. Bij laag water schrapen zij de schelpen van de zeebodem met een soort RVS-korf. Die korf zit aan een lange steel, waardoor ze een groter werkgebied hebben van soms wel 20 meter. Als ze de korf binnenhalen, zitten er niet alleen schelpen in, maar hangen er ook planten aan. Lichamelijk zwaar werk. Als het laag tij voorbij is, varen ze naar de visafslag waar de schelpen verhandeld worden.`,
+    `De bootjes zien er goed onderhouden uit. Het is serieuze business, maar of het een vetpot is?`
+  ],
+  photos: [
+    {
+      src: photo1,
+      title: "Schelpen vissen bij laag water."
+    },
+    {
+      src: photo2,
+      title: "De korf wordt over de bodem getrokken."
+    },
+    {
+      src: photo3,
+      title: "De vangst gaat in mandjes die op de visafslag verhandeld worden."
+    },
+    {
+      src: photo4,
+      title: "Bij hoog water ligt de haven weer vol; de korven klaar voor de volgende dag."
+    },
+    {
+      src: photo5,
+      title: "Ook bewoners zoeken schelpen op het strand."
+    },
+  ]
+};
--- src/data.js
+++ src/data.js
@@ -17,8 +17,10 @@ import article1014 from "./assets/data/logboek/1014";
 import article1015 from "./assets/data/logboek/1015";
 import article1016 from "./assets/data/logboek/1016";
 import article1017 from "./assets/data/logboek/1017";
+import article1018 from "./assets/data/logboek/1018";
 
 const articles = [
+  article1018,
   article1017,
   article1016,
   article1015,
