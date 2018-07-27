651fe56f72f3ccd41a8b823c17eaaef6c44ec022
godspeedelbow <eelke@boezeman.com>
2018-07-27 21:36:36 +0200

    article 1020

---
+++ src/assets/data/logboek/1020.js
@@ -0,0 +1,28 @@
+import photo1 from "../photos/1020-27-juli-2018-ankerplek-cies-800px.jpg";
+import photo2 from "../photos/1020-27-juli-2018-ankerplek-cies-2-800px.jpg";
+
+export default {
+  id: "1020",
+  title: "Wat zou jij doen?",
+  created: "27 juli 2018",
+  location: "Islas Cies",
+  geolocation: {
+    lat: 42.21666667,
+    lng: -8.90000000
+  },
+  paragraphs: [
+    `Een van de mooiste ankerplekjes van heel Galicië is bij het eiland Cies (spreek uit: Sie-jès). We besluiten er twee nachten te ankeren.`,
+    `De eerste nacht is altijd nog even wennen. Gelukkig heeft onze plotter ankeralarm, d.w.z. dat het apparaat begint te piepen zodra je te ver verwijderd raakt van je anker. We slapen licht, maar we liggen als een huis.`,
+    `De volgende dag willen we met de dinghy naar wal om op het eiland te wandelen. Helaas begint tijdens het ontbijt de wind flink door te staan naar 5 à 6 bft, inclusief irritante golven, en dan laten wij ons bootje Bess niet alleen. Dat duurt tot vroeg in de avond, daarna zakt de wind in en kalmeert de zee.`,
+    `'s Nachts zetten wij weer ons ankeralarm. Dat gaat rond 5 uur 's ochtends opeens af. We blijken 180 graden gedraaid te zijn. Het is nog pikkedonker buiten, maar de zee is rustig. Wat nu te doen? Stellen we het ankeralarm opnieuw in vanaf de nieuwe positie? Gaan we er maar van uit dat alles goed gaat met ons anker? Of moeten we een andere oplossing bedenken?`,
+    `Wat zou jij doen? Je kunt ons een mailtje sturen met jouw suggestie(s). We komen er binnenkort op terug.`,
+  ],
+  photos: [
+    {
+      src: photo1,
+    },
+    {
+      src: photo2,
+    },
+  ]
+};
