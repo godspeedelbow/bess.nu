bd94af779a8752cf622f3ba8d49aaf82005cc434
godspeedelbow <eelke@boezeman.com>
2018-09-24 22:11:04 +0200

    article 1028

---
+++ src/assets/data/logboek/1028.js
@@ -0,0 +1,30 @@
+import photo1 from "../photos/1028-22-september-2018-woonhuis-van-de-cantinho-alberto-caeiro-800px.jpg";
+import photo2 from "../photos/1028-22-september-2018-uitzicht-op-de-oceaan-800px.jpg";
+
+export default {
+  id: "1028",
+  title: "Cabo Espichel",
+  created: "22 september 2018",
+  location: "Cascais",
+  geolocation: {
+    lat: 38.69972222,
+    lng: -9.42444444
+  },
+  paragraphs: [
+    `De kaap ten zuiden van Lissabon is de Cabo Espichel. Meestal zien we die kapen vanaf het water. Maar deze zien we vanaf het land`,
+    `Een oud-collega heeft op de Cabo Espichel een stuk grond. Om haar te bezoeken nemen we de trein naar Lissabon, de ferry over de Taag naar de zuidelijke rivieroever, de bus naar Santana. Daar worden we met de auto opgehaald en is het nog een kwartier rijden naar de Cantinho Alberto Caeiro. En dan zit je ook echt in de middle-of-nowhere`,
+    `De collega en haar Portugese vriend wonen daar in een houten huisje op palen, met uitzicht op de oceaan. Ze hebben honden, poezen en een gans.`,
+    `Op het land staan drie caravans en een boomhuisje dat ze verhuren. Alle voorzieningen zijn eco-vriendelijk: gebruik van zonne-energie, toiletten waarbij plas en poep met behulp van stro gebruikt kunnen worden voor compost, iedere caravan/boomhuisje heeft een buiten-douche met water dat door de zon verwarmd wordt. Beetje primitief, maar dat wordt ruimschoots goedgemaakt door de kleurrijke inrichting, de enorme plekken, het uitzicht op de oceaan en de totale rust. Het is vijf minuten lopen naar het strand. Vlak bij de kust is het land begroeid met ijsbloemen. Die zijn nu uitgebloeid, maar in mei moet het een wonder van schoonheid zijn`,
+    `Voor foto's, zie hun website: http://cantinhocaeiro.wixsite.com/cantinhocaeiro/tree-caravans`,
+  ],
+  photos: [
+    {
+      src: photo1,
+      title: "Woonhuis van de Cantinho Alberto Caeiro."
+    },
+    {
+      src: photo2,
+      title: "Uitzicht op de oceaan."
+    },
+  ]
+};
--- src/data.js
+++ src/data.js
@@ -27,8 +27,10 @@ import article1024 from "./assets/data/logboek/1024";
 import article1025 from "./assets/data/logboek/1025";
 import article1026 from "./assets/data/logboek/1026";
 import article1027 from "./assets/data/logboek/1027";
+import article1028 from "./assets/data/logboek/1028";
 
 const articles = [
+  article1028,
   article1027,
   article1026,
   article1025,
