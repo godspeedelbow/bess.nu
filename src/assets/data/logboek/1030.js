affbdda81b35676e5ca5d0698445ea08cc97eba6
godspeedelbow <eelke@boezeman.com>
2018-09-28 07:30:46 +0200

    article 1030

---
+++ src/assets/data/logboek/1030.js
@@ -0,0 +1,55 @@
+import photo1 from "../photos/1030-24-september-2018-de-verdedigingstorens-van-mouros-castelo-1-800px.jpg";
+import photo2 from "../photos/1030-24-september-2018-de-verdedigingstorens-van-mouros-castelo-2-800px.jpg";
+import photo3 from "../photos/1030-24-september-2018-de-verdedigingstorens-van-mouros-castelo-3-800px.jpg";
+import photo4 from "../photos/1030-24-september-2018-palacio-da-pena-zomerresidentie-van-de-laatste-koningen-van-portugal-1-800px.jpg";
+import photo5 from "../photos/1030-24-september-2018-palacio-da-pena-zomerresidentie-van-de-laatste-koningen-van-portugal-2-800px.jpg";
+import photo6 from "../photos/1030-24-september-2018-palacio-da-pena-zomerresidentie-van-de-laatste-koningen-van-portugal-3-800px.jpg";
+import photo7 from "../photos/1030-24-september-2018-prachtig-betegelde-binnenplaats-800px.jpg";
+import photo8 from "../photos/1030-24-september-2018-rijk-gedecoreerd-van-binnen-800px.jpg";
+
+export default {
+  id: "1030",
+  title: "Mouros Castelo en Palácio da Pena",
+  created: "24 september 2018",
+  location: "Cascais",
+  geolocation: {
+    lat: 38.69972222,
+    lng: -9.42444444
+  },
+  paragraphs: [
+    `Ten noorden van Cascais ligt een bergrug. Daar is het koeler dan in Lissabon. In de middeleeuwen bouwen krijgsheren er kastelen. Vorsten en kooplieden hebben in de afgelopen eeuwen daar hun zomerpaleizen gebouwd. Het is teveel om in een dag te bekijken en we besluiten ons tot twee bouwwerken te beperken. We pakken de bus naar Sintra en stappen daar over op de bus naar het Mouros Castelo (moors kasteel) en het Palácio da Pena.`,
+    `We zijn niet de enigen. In Sintra staat bij de halte een rij van 75 meter; mensen die allemaal dezelfde bus willen nemen als wij. Dit is massatoerisme waar we niet helemaal op voorbereid zijn, de Portugezen duidelijk wel. Als de bus vol is en wegrijdt, komt de volgende alweer voorrijden.`,
+    `De bus brengt ons omhoog via nauwe steile weggetjes. We stappen uit bij het Mouros Castelo, nu een archelogische site. Het is nog vroeg en niet druk, dus kunnen we op ons gemak de ruïnes beklimmen. Dit waren in de middeleeuwen de eerste verdedigingslinies van Lissabon. Maar er zijn ook sporen gevonden van islamitische bewoning vóór die tijd (10e eeuw).`,
+    `Daarna lopen we door naar het Palácio da Pena. Daar is het een drukte van belang. We staan drie kwartier in de rij om het paleis binnen te komen. Eenmaal binnen dringt het tot ons door dat dit paleis het paleis is waar de laatste koningen van Portugal in de zomer verbleven, tot 1910 toen de Oktoberrevolutie een einde maakte aan de Portugese monarchie. Stapje voor stapje worden we door het paleis geleid. We zien prachtige architectuur, mooie betegelde en beschilderde muren en een overdaad aan zilver en stijlvolle meubelen. Dit is het rijke verleden van Portugal van een eeuw geleden. Daarbij valt paleis Het Loo in het niet.`,
+  ],
+  photos: [
+    {
+      src: photo1,
+    },
+    {
+      src: photo2,
+    },
+    {
+      src: photo3,
+      title: "De verdedigingstorens van Mouros Castelo."
+    },
+    {
+      src: photo4,
+    },
+    {
+      src: photo5,
+    },
+    {
+      src: photo6,
+      title: "Palácio da Pena; zomerresidentie van de laatste koningen van Portugal."
+    },
+    {
+      src: photo7,
+      title: "Prachtig betegelde binnenplaats."
+    },
+    {
+      src: photo8,
+      title: "Rijk gedecoreerd van binnen."
+    },
+  ]
+};
