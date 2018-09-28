6be6587ced4fc3ab4682fb693f5e4c4be3963921
godspeedelbow <eelke@boezeman.com>
2018-09-28 07:17:02 +0200

    article 1029

---
+++ src/assets/data/logboek/1029.js
@@ -0,0 +1,17 @@
+export default {
+  id: "1029",
+  title: "Who is the captain?",
+  created: "22 september 2018",
+  location: "Cascais",
+  geolocation: {
+    lat: 38.69972222,
+    lng: -9.42444444
+  },
+  paragraphs: [
+    `Als we een haven binnenlopen, moeten de boot- en verzekeringspapieren ingeleverd worden en een formulier ingevuld: hoe groot is uw schip, waar komt u vandaan, waar gaat u naartoe, wie is de eigenaar en wie is de kapitein? Net zoals bij het afvaren en aanleggen, doen we deze papierhandel altijd om de beurt.`,
+    `In Cascais is het mijn (Ineke) beurt. De dame achter de receptie vraagt keurig: who is the owner of the ship? Me. And who is the captain? Also me. De dame begint te glimmen. Heel goed, een vrouw als kapitein. En ik glim mee.`,
+    `Na mij komt een jonge vrouw aan de beurt. Ze bedankt de dames van de receptie voor hun gastvrijheid en meldt dat ze binnenkort ankerop gaat. Ze is de kapitein van de driemaster Europa die net buiten de haven voor anker ligt. Ik vraag of ze een volle bak heeft en wat haar bestemming is.`,
+    `De Europa heeft 48 gasten aan boord en een 20-koppige bemanning.  Volgende bestemming: Tenerife (Canarische Eilanden). Eindbestemming: via Montevideo (Argentinië) naar Antartica. Wow, dan ben je een echte kapitein.`,
+    `Voor de Europa, zie https://www.barkeuropa.nl`,
+  ],
+};
