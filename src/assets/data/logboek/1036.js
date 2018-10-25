e7d51101630f922e749ee63475c581206c24bf3c
Eelke Boezeman <eboezeman@apple.com>
2018-10-25 06:57:39 +0200

    article 1036

---
+++ src/assets/data/logboek/1036.js
@@ -0,0 +1,48 @@
+import photo1 from "../photos/1036-23-oktober-bess-uit-het-water-800px.jpg";
+import photo2 from "../photos/1036-23-oktober-vissenstrepen-als-trambanen-op-de-romp-800px.jpg";
+import photo3 from "../photos/1036-23-oktober-de-saildrive-zit-helemaal-vol-met-kleine-schelpjes-of-pokken-800px.jpg";
+import photo4 from "../photos/1036-23-oktober-poepslurpers-rond-de-boot-800px.jpg";
+import photo5 from "../photos/1036-23-oktober-lex-met-de-pokkenslijper-800px.jpg";
+import photo6 from "../photos/1036-23-oktober-ineke-klaart-een-klus-800px.jpg";
+
+export default {
+  id: "1036",
+  title: "Poepslurpers en pokkenslijpers",
+  created: "23 oktober 2018",
+  location: "Lagos",
+  geolocation: {
+    lat: 37.10916667,
+    lng: -8.67611111
+  },
+  paragraphs: [
+    `Bess gaat de wal op en we zijn reuzebenieuwd hoeveel aangroei er onder de boot hangt. Tot onze verbazing is de onderkant schoon. We zien overal strepen op de romp en de man van de haven vertelt dat die strepen door vissen gemaakt worden. Het ziet er uit alsof ze zich "schuren" aan de romp, maar waarom er dan twee parallelle strepen staan?`,
+    `Zodra je de Golf van Biskaje over bent, is het water in de havens superhelder. Je ziet in elke haven dieren: zeesterren, vissen, kwallen. Die vissen noemen we 'poepslurpers'; ze houden de haven schoon van uitwerpselen van de boten. Maar blijkbaar houden ze ook van de onderkant van de boot.`,
+    `Zo schoon als de romp is, zo veel aangroei zit er op de saildrive (het staartstuk van de motor) en de schroef. De pokken en schelpen hebben er een gerieflijk onderkomen gevonden. Pokken die aangroeien zijn knetterhard. De jongens van de haven vertellen ons dat we de saildrive en de schroefbladen schoon kunnen maken met een speciale nylon slijpschijf, door ons gedoopt tot 'pokkenslijper'. Daarna kan de saildrive in de primer en anti-aangroeiverf; een nieuwe klus op onze lijst. Kost een paar dagen, maar dan is het ook weer blingbling onder water.`,
+  ],
+  photos: [
+    {
+      src: photo1,
+      title: 'Bess uit het water'
+    },
+    {
+      src: photo2,
+      title: 'Vissenstrepen als trambanen op de romp'
+    },
+    {
+      src: photo3,
+      title: 'De saildrive zit helemaal vol met kleine schelpjes of pokken'
+    },
+    {
+      src: photo4,
+      title: 'Poepslurpers rond de boot'
+    },
+    {
+      src: photo5,
+      title: 'Lex met de pokkenslijper'
+    },
+    {
+      src: photo6,
+      title: 'Ineke klaart een klus'
+    },
+  ]
+};
--- src/data.js
+++ src/data.js
@@ -34,8 +34,10 @@ import article1032 from "./assets/data/logboek/1032";
 import article1033 from "./assets/data/logboek/1033";
 import article1034 from "./assets/data/logboek/1034";
 import article1035 from "./assets/data/logboek/1035";
+import article1036 from "./assets/data/logboek/1036";
 
 const articles = [
+  article1036,
   article1035,
   article1034,
   article1033,
